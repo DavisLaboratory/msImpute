3438f58e318ccede8f2298a657eb1aa3257278b2
soroorh <soroorh@student.unimelb.edu.au>
2020-10-21 00:19:44 +1100

    v.99.23

--- R/msImpute.R
+++ R/msImpute.R
@@ -1,51 +1,123 @@
-#' Peptide-level imputation in mass spectrometry label-free proteomics by low-rank approximation
+#' Imputation of peptide log-intensity in mass spectrometry label-free proteomics by low-rank approximation
 #'
-#' Returns a completed peptide intensity matrix where missing values (NAs) are imputated
+#' Returns a completed matrix of peptide log-intensity where missing values (NAs) are imputated
 #' by low-rank approximation of the input matrix. Non-NA entries remain unmodified. \code{msImpute} requires at least 4
 #' non-missing measurements per peptide across all samples. It is assumed that peptide intensities  (DDA), or MS1/MS2 normalised peak areas (DIA),
-#' are log2-transformed and normalised (e.g. quantile normalisation).
+#' are log2-transformed and normalised (e.g. by quantile normalisation).
 #'
-#' \code{msImpute} operates on the softImpute-ALS algorithm.
-#' For more details on the underlying algorithm, please see \code{\link[softImpute]{softImpute}} package.
+#' @details
 #'
-#' @param object Numeric matrix giving log-intensity where missing values are denoted by NA. Rows are peptides, columns are samples.
-#' @param rank.max Numeric. This restricts the rank of the solution. is set to min(dim(\code{object})-1) by default.
+#' \code{msImpute} operates on the \code{softImpute-als} algorithm in \code{\link[softImpute]{softImpute}} package.
+#' The algorithm estimates a low-rank matrix ( a smaller matrix
+#' than the input matrix) that approximates the data with a reasonable accuracy. \code{SoftImpute-als} determines the optimal
+#' rank of the matrix through the \code{lambda} parameter, which it learns from the data.
+#' This algorithm is implemented in \code{method="v1"}.
+#' In v2 we have used a information theoretic approach to estimate the optimal rank, instead of relying on \code{softImpute-als}
+#' defaults. Similarly, we have implemented a new approach to estimate \code{lambda} from the data. Low-rank approximation
+#' is a linear reconstruction of the data, and is only appropriate for imputation of MAR data. In order to make the
+#' algorithm applicable to MNAR data, we have implemented \code{method="v2-mnar"} which imputes the missing observations
+#' as weighted sum of values imputed by msImpute v2 (\code{metho="v2"}) and random draws from a Gaussian distribution.
+#' Missing values that tend to be missing completely in one or more experimental groups will be weighted more (shrunken) towards
+#' imputation by sampling from a Gaussian parameterised by smallest observed values in the sample (similar to minProb, or
+#' Perseus). However, if the missing value distribution is even across the samples for a peptide, the imputed values
+#' for that peptide are shrunken towards
+#' low-rank imputed values. The judgment of distribution of missing values is based on the EBM metric implemented in
+#' \code{selectFeatures}, which is also a information theory measure.
+#'
+#'
+#' @param y Numeric matrix giving log-intensity where missing values are denoted by NA. Rows are peptides, columns are samples.
+#' @param method character. Allowed values are \code{"v2"} for \code{msImputev2} imputation (enhanced version) for MAR.
+#' \code{method="v2-mnar"} (modified low-rank approx for MNAR), and \code{"v1"} initial release of \code{msImpute}
+#' @param group character or factor vector of length \code{ncol(y)}
+#' @param a numeric. the weight parameter. default to 0.2.
+#' @param rank.max Numeric. This restricts the rank of the solution. is set to min(dim(\code{y})-1) by default in "v1".
 #' @param lambda Numeric. Nuclear-norm regularization parameter. Controls the low-rank property of the solution
 #' to the matrix completion problem. By default, it is determined at the scaling step. If set to zero
-#' the algorithm reverts to "hardImputation", where the convergence will be slower.
-#' @param thresh Numeric. Convergence threshold. Set to 1e-05, by default.
+#' the algorithm reverts to "hardImputation", where the convergence will be slower. Applicable to "v1" only.
+#' @param thresh Numeric. Convergence threshold. Set to 1e-05, by default. Applicable to "v1" only.
 #' @param maxit Numeric. Maximum number of iterations of the algorithm before the algorithm is converged. 100 by default.
+#' Applicable to "v1" only.
 #' @param trace.it Logical. Prints traces of progress of the algorithm.
+#' Applicable to "v1" only.
 #' @param warm.start List. A SVD object can be used to initialize the algorithm instead of random initialization.
-#' @param final.svd  Logical. Shall final SVD object be saved? The solutions to the matrix completion problems are computed from U, D and V components of final SVD.
+#' Applicable to "v1" only.
+#' @param final.svd  Logical. Shall final SVD object be saved?
+#' The solutions to the matrix completion problems are computed from U, D and V components of final SVD.
+#' Applicable to "v1" only.
+#' @param biScale_maxit number of iteration for the scaling algorithm to converge . See \code{scaleData}. You may need to change this
+#' parameter only if you're running \code{method=v1}. Applicable to "v1" only.
 #' @return Missing values are imputed by low-rank approximation of the input matrix. If input is a numeric matrix,
 #' a numeric matrix of identical dimensions is returned.
 #'
+#'
 #' @examples
-#' set.seed(101)
-#' n=12000
-#' p=10
-#' J=5
-#' np=n*p
-#' missfrac=0.3
-#' x=matrix(rnorm(n*J,mean = 5,sd = 0.2),n,J)%*%matrix(rnorm(J*p, mean = 5,sd = 0.2),J,p)+
-#'   matrix(rnorm(np,mean = 5,sd = 0.2),n,p)/5
-#' ix=seq(np)
-#' imiss=sample(ix,np*missfrac,replace=FALSE)
-#' xna=x
-#' xna[imiss]=NA
-#' keep <- (rowSums(!is.na(xna)) >= 4)
-#' xna <- xna[keep,]
-#' xna <- scaleData(xna)
-#' xcomplete <- msImpute(object=xna)
-#' @seealso selectFeatures, scaleData
+#' data(pxd010943)
+#' y <- log2(data.matrix(pxd010943))
+#' group <- gsub("_[1234]","", colnames(y))
+#' yimp <- msImpute(y, method="v2-mnar", group=group)
+#' @seealso selectFeatures
+#' @author Soroor Hediyeh-zadeh
 #' @references
 #' Hastie, T., Mazumder, R., Lee, J. D., & Zadeh, R. (2015). Matrix completion and low-rank SVD via fast alternating least squares. The Journal of Machine Learning Research, 16(1), 3367-3402.
 #' @references
 #' Hediyeh-zadeh, S., Webb, A. I., & Davis, M. J. (2020). MSImpute: Imputation of label-free mass spectrometry peptides by low-rank approximation. bioRxiv.
 #' @importFrom methods is
 #' @export
-msImpute <- function(object, rank.max = NULL, lambda = NULL, thresh = 1e-05,
+msImpute <- function(y, method=c("v2-mnar", "v2", "v1"),
+                     group = NULL,
+                     a = 0.2,
+                     rank.max = NULL, lambda = NULL, thresh = 1e-05,
+                     maxit = 100, trace.it = FALSE, warm.start = NULL,
+                     final.svd = TRUE, biScale_maxit=20) {
+
+  method <- match.arg(method, c("v2-mnar","v2", "v1"))
+
+
+  if(any(is.nan(y) | is.infinite(y))) stop("Inf or NaN values encountered.")
+  if(any(rowSums(!is.na(y)) <= 3)) stop("Peptides with excessive NAs are detected. Please revisit your fitering step. At least 4 non-missing measurements are required for any peptide.")
+  if(any(y < 0, na.rm = TRUE)){
+    warning("Negative values encountered in imputed data. Please consider revising filtering and/or normalisation steps.")
+  }
+
+
+
+
+  if(method=="v1"){
+    message(paste("Running msImpute version", method))
+    yimp <- scaleData(y, maxit = biScale_maxit)
+    yimp <- msImputev1(yimp,
+                       rank.max = rank.max, lambda = lambda, thresh = thresh,
+                       maxit = maxit, trace.it = trace.it, warm.start = warm.start,
+                       final.svd = final.svd)
+  }else{
+    message(paste("Running msImpute version", method))
+    yimp <- msImputev1(y, rank.max = ceiling(erank(y)), lambda = estimateLambda(y))
+    if (method == "v2-mnar"){
+      #message(paste("Running msImpute version", method))
+      if (is.null(group)) stop("Please specify the 'group' argument. This is required for the 'v2-mnar' method.")
+      ygauss <- gaussimpute(y)
+      yimp <- l2bary(y=y, ygauss = ygauss, yerank = yimp, group = group, a=a)
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+  return(yimp)
+
+
+}
+
+
+#' @importFrom methods is
+#' @keywords internal
+msImputev1 <- function(object, rank.max = NULL, lambda = NULL, thresh = 1e-05,
                      maxit = 100, trace.it = FALSE, warm.start = NULL, final.svd = TRUE) {
   # data scaled by biScale
   if(is(object,"list")) {
@@ -55,10 +127,9 @@ msImpute <- function(object, rank.max = NULL, lambda = NULL, thresh = 1e-05,
 
   # data is not scaled by biscale
   if(is(object, "matrix")) {
-    x <- object
-    xnas <- x
-    warning("Input is not scaled. Data scaling is recommended for msImpute optimal performance.")
-    }
+    xnas <- x <- object
+    #warning("Input is not scaled. Data scaling is recommended for msImpute optimal performance.")
+  }
   # MAList object
   # or \code{MAList} object from \link{limma}
   # if(is(object,"MAList")) x <- object$E
@@ -69,25 +140,89 @@ msImpute <- function(object, rank.max = NULL, lambda = NULL, thresh = 1e-05,
     warning("Negative values encountered in imputed data. Please consider revising filtering and/or normalisation steps.")
   }
   if(is.null(rank.max)) rank.max <- min(dim(x) - 1)
-  cat("maximum rank is", rank.max, "\n")
-  cat("computing lambda0 ... \n")
+  message(paste("rank is", rank.max))
+  message("computing lambda0 ...")
   if(is.null(lambda)) lambda <- softImpute::lambda0(xnas)
-  cat("lambda0 is", lambda, "\n")
-  cat("fit the low-rank model ... \n")
-  fit <- softImpute::softImpute(xnas,rank=rank.max,lambda=lambda, type = "als", thresh = thresh,
-                  maxit = maxit, trace.it = trace.it, warm.start = warm.start, final.svd = final.svd)
-  cat("model fitted. \nImputting missing entries ... \n")
+  message(paste("lambda0 is", lambda))
+  message("fit the low-rank model ...")
+  fit <- softImpute::softImpute(x, rank.max=rank.max,lambda=lambda, type = "als", thresh = thresh,
+                                maxit = maxit, trace.it = trace.it, warm.start = warm.start, final.svd = final.svd)
+  message("model fitted. \nImputting missing entries ...")
   ximp <- softImpute::complete(x, fit)
-  cat("Imputation completed \n")  # need to define a print method for final rank model fitted
+  message("Imputation completed")  # need to define a print method for final rank model fitted
 
   return(ximp)
-#
-#   if(is(object,"MAList")) {
-#     object$E <- ximp
-#     return(object)
-#   }else{
-#     return(ximp)
-#   }
+  #
+  #   if(is(object,"MAList")) {
+  #     object$E <- ximp
+  #     return(object)
+  #   }else{
+  #     return(ximp)
+  #   }
 
 
 }
+
+#' @keywords internal
+eigenpdf <- function(y, rank=NULL){
+  s <- softImpute::softImpute(y, rank.max = ifelse(!is.null(rank), rank, min(dim(y)-1)), lambda =0)$d
+  return(s/sum(abs(s)))
+}
+
+
+#' @importFrom stats var sd
+#' @keywords internal
+estimateS0 <- function(y, rank=NULL){
+  set.seed(123)
+  s0 <- vector(length = 100L)
+  for(i in seq_len(100)){
+    s0[i] <- var(eigenpdf(y, rank=rank))
+  }
+  return(list("s0" = mean(s0), "s0.1sd"= (mean(s0) + sd(s0))))
+}
+
+#' @keywords internal
+erank <- function(y) {
+  P <- eigenpdf(y, rank = NULL)
+  return(exp(-sum(P*log(P)))) # shannon entropy
+}
+
+
+#' @keywords internal
+estimateLambda <- function(y, rank=NULL) mean(matrixStats::colSds(y, na.rm = TRUE))/estimateS0(y, rank=rank)$"s0.1sd"
+
+
+#' @importFrom stats quantile
+#' @keywords internal
+l2bary <- function(y, ygauss, yerank, group, a=0.2){
+  pepVars <- matrixStats::rowSds(y, na.rm = TRUE)
+  varq75 <- quantile(pepVars, p = 0.75)
+  #varq75 <- mean(pepVars)
+  EBM <- ebm(y, group)
+  yl2 <- list()
+  for(j in colnames(y)){
+    # if entropy is nan and variance is high, it is most likely biological missing
+    w1 <- ifelse(is.nan(EBM) & (pepVars > varq75), 1-a, a)
+    w2 <- 1-w1
+    yl2[[j]] <- rowSums(c(w1,w2)*cbind(ygauss[,j], yerank[,j]))
+  }
+
+  yl2 <- do.call(cbind, yl2)
+  yl2[!is.na(y)] <- y[!is.na(y)]
+  return(yl2)
+
+
+}
+
+#' @keywords internal
+gaussimpute <- function(x, width=0.3, shift=1.8) {
+  # distributions are induced by measured values in each sample
+  data.mean <- colMeans(x, na.rm = TRUE)
+  data.sd <- matrixStats::colSds(x, na.rm = TRUE)
+  n <- nrow(x)
+  z <- mvtnorm::rmvnorm(n, mean = data.mean - shift*data.sd , sigma = diag(data.sd*width))
+  x[is.na(x)] <- z[is.na(x)]
+  return(x)
+}
+
+
--- R/selectFeatures.R
+++ R/selectFeatures.R
@@ -1,35 +1,46 @@
-#' Select features with high biological dropout rate
+#' Select features for MAR/MNAR pattern examination
 #'
-#' Fits a linear model to peptide dropout rate against peptide abundance. The selected features (peptides) can be
-#' used to determine if data is Missing Not At Random (MNAR). Users should note that \code{msImpute} assumes peptides
-#' are Missing At Random (MAR).
+#' Two methods are provided to identify features (peptides or proteins) that can be informative of missing patterns.
+#' Method \code{hvp} fits a linear model to peptide dropout rate (proportion of samples were peptide is missing)
+#' against peptide abundance (average log2-intensity). Method \code{emb} is a information theoretic approach to
+#' identify missing patterns. It quantifies the heterogeneity (entropy) of missing patterns per
+#' biological (experimental group). This is the default method.
 #'
-#' @param object Numeric matrix giving log-intensity where missing values are denoted by NA.
+#' @details
+#' In general, the presence of group-wise (structured) blocks of missing values,
+#' where peptides are missing in one experimental group can indicate MNAR, whereas if
+#' such patterns are absent (or missingness is uniform across the samples), peptides are likely MAR.
+#' In the presence of MNAR, left-censored MNAR imputation methods should
+#' be chosen. Two methods are provided to explore missing patterns: \code{method=hvp} identifies top \code{n_features}
+#' peptides with high average expression that also have high dropout rate, defined as the proportion of samples where
+#' peptide is missing. Peptides with high (potentially) biological dropouts are marked in the \code{hvp} column in the
+#' output dataframe. This method does not use any information about experimental conditions (i.e. group).
+#' Another approach to explore and quantify missing patterns is by looking at how homogeneous or heterogeneous
+#' missing patterns are in each experimental group. This is done by computing entropy of distribution of observed values.
+#' This is the default and recommended method for \code{selectFeatures}. Entropy is reported in \code{EBM} column
+#' of the output. A \code{NaN} EBM indicates peptide is missing at least in one experimental group. Features set to
+#' \code{TRUE} in \code{msImpute_feature} column are the features selected by the selected method. Users are encouraged
+#' to use the EBM metric to find informative features, hence why the \code{group} argument is required.
+#'
+#'
+#'
+#' @param x Numeric matrix giving log-intensity where missing values are denoted by NA.
 #' Rows are peptides, columns are samples.
-#' @param n_features Numeric, number of features with high dropout rate. 500 by default.
-#' @param suppress_plot Logical show plot of dropouts vs abundances.
+#' @param method character. What method should be used to find features? options include \code{method='hvp'} and \code{method='ebm'}
+#' @param group character or factor vector specifying biological (experimental) group e.g. control, treatment, WT, KO
+#' @param n_features Numeric, number of features with high dropout rate. 500 by default. Applicable if \code{method="hvp"}.
+#' @param suppress_plot Logical show plot of dropouts vs abundances. Default to TRUE. Applicable if \code{method="hvp"}.
 #'
 #' @return A data frame with a logical column denoting the selected features
 #'
 #' @examples
-#' set.seed(101)
-#' n=12000
-#' p=10
-#' J=5
-#' np=n*p
-#' missfrac=0.3
-#' x=matrix(rnorm(n*J,mean = 5,sd = 0.2),n,J)%*%matrix(rnorm(J*p, mean = 5,sd = 0.2),J,p)+
-#'   matrix(rnorm(np,mean = 5,sd = 0.2),n,p)/5
-#' ix=seq(np)
-#' imiss=sample(ix,np*missfrac,replace=FALSE)
-#' xna=x
-#' xna[imiss]=NA
-#' keep <- (rowSums(!is.na(xna)) >= 4)
-#' xna <- xna[keep,]
-#' rownames(xna) <- 1:nrow(xna)
-#' hdp <- selectFeatures(xna, n_features=500,  suppress_plot=FALSE)
+#' data(pxd007959)
+#' group <- pxd007959$samples$group
+#' y <- data.matrix(pxd007959$y)
+#' y <- log2(y)
+#' hdp <- selectFeatures(y, method="ebm", group = group)
 #' # construct matrix M to capture missing entries
-#' M <- ifelse(is.na(xna),1,0)
+#' M <- ifelse(is.na(y),1,0)
 #' M <- M[hdp$msImpute_feature,]
 #' # plot a heatmap of missingness patterns for the selected peptides
 #' require(ComplexHeatmap)
@@ -41,7 +52,7 @@
 #'               show_column_names = TRUE,
 #'               cluster_rows = TRUE,
 #'               cluster_columns = TRUE,
-#'               show_column_dend = FALSE,
+#'               show_column_dend = TRUE,
 #'               show_row_dend = FALSE,
 #'               row_names_gp =  gpar(fontsize = 7),
 #'               column_names_gp = gpar(fontsize = 8),
@@ -51,20 +62,15 @@
 #'               legend_width = unit(6, "cm")),
 #'          )
 #' hm <- draw(hm, heatmap_legend_side = "left")
-#' @seealso scaleData, msImpute
+#' @author Soroor Hediyeh-zadeh
+#' @seealso msImpute
 #' @references
 #' Hediyeh-zadeh, S., Webb, A. I., & Davis, M. J. (2020). MSImpute: Imputation of label-free mass spectrometry peptides by low-rank approximation. bioRxiv.
 #' @importFrom stats lm residuals
 #' @importFrom methods is
 #' @importFrom graphics abline plot
 #' @export
-selectFeatures <- function(object, n_features=500, suppress_plot = FALSE) {
-
-  if(is(object,"MAList")){
-    x <- object$E
-  }else{
-    x <- object
-  }
+selectFeatures <- function(x, method=c("ebm","hvp"), group, n_features=500, suppress_plot = TRUE) {
 
   if(is.null(rownames(x))) stop("No row names in input. Please provide input with named rows.")
   if(any(is.nan(x) | is.infinite(x))) stop("Inf or NaN values encountered.")
@@ -76,20 +82,57 @@ selectFeatures <- function(object, n_features=500, suppress_plot = FALSE) {
   resids <- residuals(linear_fit)
   lin_res_o <- order(resids, decreasing = TRUE)
 
-  cols <- rep("#3E71A8", length(resids))
-  cols[lin_res_o[seq_len(n_features)]] <- "#DE1A1A"
+  # Entropy of batch mixing----
+  EBM <- ebm(x=x,group=group)
 
-  if(!suppress_plot){
+
+  # default method is ebm
+  method <- match.arg(method, c("ebm","hvp"))
+
+  if(!suppress_plot & method=="hvp"){
+    cols <- rep("#3E71A8", length(resids))
+    cols[lin_res_o[seq_len(n_features)]] <- "#DE1A1A"
     plot(x = AveExpr, y = dropout, pch = 16,
          cex = 0.5, col = cols, main = paste("Top ",n_features," high droupout peptides", sep =""))
     abline(linear_fit)
   }
 
-  hdrp <- data.frame(name = rownames(x), AveExpr = AveExpr, dropout = dropout ,
-                         residual = resids, msImpute_feature=FALSE)
-  hdrp$msImpute_feature[lin_res_o[seq_len(n_features)]] <- TRUE
+  hdrp <- data.frame(name = rownames(x), AveExpr = AveExpr, dropout = dropout,
+                         residual = resids, hvp=FALSE, EBM=EBM, msImpute_feature=FALSE)
+
+
+  hdrp$hvp[lin_res_o[seq_len(n_features)]] <- TRUE
+
+
+  if(method=="hvp"){
+    hdrp$msImpute_feature[lin_res_o[seq_len(n_features)]] <- TRUE
+  }
+
+  if(method=="ebm"){
+    if(all(!is.nan(EBM))){
+      message("No NaN EBMs detected. Peptides are missing evenly across samples.")
+      message("Switchted to 'hvp' method as final msImpute features")
+      hdrp$msImpute_feature[lin_res_o[seq_len(n_features)]] <- TRUE
+    }else{
+      hdrp$msImpute_feature[is.nan(EBM)] <- TRUE
+    }
+
+  }
+
   hdrp <- data.table::as.data.table(hdrp)
 
   return(hdrp)
 
+}
+
+#' @keywords internal
+ebm <- function(x, group){
+  M <- ifelse(is.na(x), 1,0)
+  P <- list()
+  for(i in unique(group)){
+    P[[i]] <- rowMeans(M[,group==i]==0)*log(rowMeans(M[,group==i]==0)) # i.e. number observed entries per group
+  }
+
+  Pmat <- do.call(cbind, P)
+  return(-rowSums(Pmat))
 }
